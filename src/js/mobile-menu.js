252ce820d13f70f4376529bf6bcab5be07c63f16
Vadilonische <vadilonische26@gmail.com>
2023-12-26 13:02:35 +0200

    upd social icon & js
    target="_blank" +
    fix code in js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,26 +1,24 @@
 (() => {
-    const refs = {
-      openMobileMenu: document.querySelectorAll('[data-mobile-menu-open]'),
-      closeMobileMenu: document.querySelector('[data-mobile-menu-close]'),
-      mobileMenu: document.querySelector('[data-mobile-menu]'),
-    };
-  
-    refs.openMobileMenu.forEach((element) => {
-      element.addEventListener('click', toggleMobileMenu);
-    });
-    refs.closeMobileMenu.addEventListener('click', toggleMobileMenu);
-  
-    //* Закривання по кліку на кнопку мобільного меню
-  
-    refs.mobileMenu.addEventListener('click', removeMobileMenu);
-    function removeMobileMenu(e) {
-      if (e.target === e.currentTarget) {
-        refs.mobileMenu.classList.add('is-hidden');
-      }
+  const refs = {
+    openMobileMenu: document.querySelectorAll('[data-mobile-menu-open]'),
+    closeMobileMenu: document.querySelector('[data-mobile-menu-close]'),
+    mobileMenu: document.querySelector('[data-mobile-menu]'),
+    closeMenuLink: document.querySelectorAll('.mobile-modal-nav-link'),
+  };
+  refs.openMobileMenu.forEach((element) => {
+    element.addEventListener('click', toggleMobileMenu);
+  });
+  refs.closeMobileMenu.addEventListener('click', toggleMobileMenu);
+  refs.closeMenuLink.forEach(item => item.addEventListener('click', toggleMobileMenu));
+  //* Закривання по кліку на кнопку мобільного меню
+  refs.mobileMenu.addEventListener('click', removeMobileMenu);
+  function removeMobileMenu(e) {
+    if (e.target === e.currentTarget) {
+      refs.mobileMenu.classList.add('is-hidden');
     }
-  
-    function toggleMobileMenu() {
-      refs.mobileMenu.classList.toggle('is-hidden');
-      document.body.classList.toggle('no-scroll');
-    }
-  })();
+  }
+  function toggleMobileMenu() {
+    refs.mobileMenu.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll');
+  }
+})();
